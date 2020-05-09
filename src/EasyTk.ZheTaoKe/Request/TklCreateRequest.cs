9776817ef118736d10fecab50907d5454c7b2d15
zyy314680012 <zyy314680012@163.com>
2020-05-09 16:12:07 +0800

    ##

---
+++ src/EasyTk.ZheTaoKe/Request/TklCreateRequest.cs
@@ -0,0 +1,41 @@
+﻿using EasyTk.ZheTaoKe.Response;
+using Newtonsoft.Json;
+
+namespace EasyTk.ZheTaoKe.Request
+{
+    public class TklCreateRequest : BaseRequest<TklCreateResponse>
+    {
+        protected override string[] GatewayArray => new[]
+        {
+            "https://api.zhetaoke.com:10001/api/open_tkl_create.ashx"
+        };
+
+        #region ============================
+
+        ///<summary> 
+        ///text string	是	口令弹框内容，长度大于5个字符，如：美美小编精心推荐。尽量不要带特殊符号或特殊词，否则生成的淘口令手淘里可能打不开。
+        ///</summary>
+        [JsonProperty("text")]
+        public string Text { get; set; }
+
+        ///<summary> 
+        ///url string	是	口令跳转目标页，如：https://uland.taobao.com/，必须以https开头，可以是二合一链接、长链接、短链接等各种淘宝高佣链接；支持渠道备案链接。请注意，该参数需要进行Urlencode编码后传入。。重要的事情说三遍：该参数需要进行Urlencode编码！该参数需要进行Urlencode编码！该参数需要进行Urlencode编码！
+        ///</summary>
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        ///<summary> 
+        ///logo string	否	口令弹框logoURL，如：https://img.alicdn.com/bao/uploaded/i2.jpg_200x200.jpg
+        ///</summary>
+        [JsonProperty("logo")]
+        public string Logo { get; set; }
+
+        ///<summary> 
+        ///signurl int	否	值为1或者2，表示返回淘宝联盟请求地址，大家拿到地址后再用自己的服务器二次请求即可获得最终结果，值为1返回http链接，值为2返回https安全链接，值为0表示直接返回淘口令最终结果。
+        ///</summary>
+        [JsonProperty("signurl")]
+        public int SignUrl { get; set; }
+
+        #endregion
+    }
+}
---
+++ src/EasyTk.ZheTaoKe/Response/TklCreateResponse.cs
@@ -0,0 +1,85 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using EasyTk.Helpers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace EasyTk.ZheTaoKe.Response
+{
+    public class TklCreateResponse : BaseResponse
+    {
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        ///<summary> 
+        ///status  200,
+        ///</summary>
+        [JsonProperty("status")]
+        public long Status { get; set; }
+
+        ///<summary> 
+        ///model  "￥mlo11oMAIFD￥"
+        ///</summary>
+        [JsonProperty("model")]
+        public string Model { get; set; }
+
+        public async Task<TbkTpwdCreateResponse> GetTklCreateResponse()
+        {
+            if (!string.IsNullOrEmpty(Model))
+            {
+                return new TbkTpwdCreateResponse
+                {
+                    Body = string.Empty,
+                    Response = new TbkTpwdCreateResponseData
+                    {
+                        RequestId = string.Empty,
+                        Data = new TbkTpwdCreateResponseResult
+                        {
+                            Model = Model
+                        }
+                    }
+                };
+            }
+
+            HttpResponseMessage response = null;
+            using (var client = HttpHelper.HttpClientFactory.CreateClient())
+            {
+                response = await client.GetAsync(Url);
+            }
+
+            if (response != null && response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                if (!string.IsNullOrEmpty(content))
+                {
+                    var result = JsonConvert.DeserializeObject<TbkTpwdCreateResponse>(content);
+                    result.Body = content;
+                    return result;
+                }
+            }
+
+            return default;
+        }
+
+        public class TbkTpwdCreateResponse : BaseResponse
+        {
+            [JsonProperty("tbk_tpwd_create_response")]
+            public TbkTpwdCreateResponseData Response { get; set; }
+        }
+
+        public class TbkTpwdCreateResponseData
+        {
+            [JsonProperty("data")]
+            public TbkTpwdCreateResponseResult Data { get; set; }
+
+            [JsonProperty("request_id")]
+            public string RequestId { get; set; }
+        }
+
+        public class TbkTpwdCreateResponseResult
+        {
+            [JsonProperty("model")]
+            public string Model { get; set; }
+        }
+    }
+}
