5f7dd0667b3710099d0ce3c005c634f75aff3e43
zyy314680012 <zyy314680012@163.com>
2020-10-28 15:36:58 +0800

    # bug

--- src/EasyTk.PinDuoDuo/Response/PddGoodsOptGetResponse.cs
+++ src/EasyTk.PinDuoDuo/Response/PddGoodsOptGetResponse.cs
@@ -18,21 +18,25 @@ namespace EasyTk.PinDuoDuo.Response
             /// <summary>
             /// level	INTEGER		层级，1-一级，2-二级，3-三级，4-四级
             /// </summary>
+            [JsonProperty("level")]
             public int Level { get; set; }
 
             /// <summary>
             /// opt_id	LONG		商品标签ID
             /// </summary>
+            [JsonProperty("opt_id")]
             public long OptId { get; set; }
 
             /// <summary>
             /// opt_name	STRING		商品标签名
             /// </summary>
+            [JsonProperty("opt_name")]
             public string OptName { get; set; }
 
             /// <summary>
             /// parent_opt_id	LONG		id所属父ID，其中，parent_id=0时为顶级节点
             /// </summary>
+            [JsonProperty("parent_opt_id")]
             public long ParentOptId { get; set; }
         }
     }
