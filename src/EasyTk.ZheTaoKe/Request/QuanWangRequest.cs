6759cae3b47299d23321ab79c12ff48aa85f36a9
henry.zhang <henry.z@vlinkglobal.com>
2022-04-11 13:35:03 +0800

    #

--- src/EasyTk.ZheTaoKe/Request/QuanWangRequest.cs
+++ src/EasyTk.ZheTaoKe/Request/QuanWangRequest.cs
@@ -26,7 +26,7 @@ namespace EasyTk.ZheTaoKe.Request
         public long? PageSize { get; set; } = 20;
 
         ///<summary> 
-        ///sort string	否	商品排序方式，new：按照综合排序，sale_num_asc：按照月销量从小到大排序，sale_num_desc：按照月销量从大到小排序，commission_rate_asc：按照佣金比例从小到大排序，commission_rate_desc：按照佣金比例从大到小排序，price_asc：按照价格从小到大排序，price_desc：按照价格从大到小排序。
+        ///商品排序方式，new：按照综合排序，total_sale_num_asc：按照总销量从小到大排序，total_sale_num_desc：按照总销量从大到小排序，sale_num_asc：按照月销量从小到大排序，sale_num_desc：按照月销量从大到小排序，commission_rate_asc：按照佣金比例从小到大排序，commission_rate_desc：按照佣金比例从大到小排序，price_asc：按照价格从小到大排序，price_desc：按照价格从大到小排序。
         ///</summary>
         [JsonProperty("sort")]
         public string Sort { get; set; }
@@ -36,6 +36,12 @@ namespace EasyTk.ZheTaoKe.Request
         ///</summary>
         [JsonProperty("q")]
         public string Q { get; set; }
+
+        /// <summary>
+        ///material_id	string	否	官方的物料Id，默认为空表示全网商品；
+        /// </summary>
+        [JsonProperty("material_id")]
+        public string MaterialId { get; set; }
 
         ///<summary> 
         ///youquan string	否	是否有券，1为有券，其它值为全部商品
@@ -65,7 +71,13 @@ namespace EasyTk.ZheTaoKe.Request
         ///itemloc string	否	商品所在地，值为空：全部商品，其它值：北京、上海、广州、深圳、重庆、杭州等。必须是城市名称，不能带省份。
         ///</summary>
         [JsonProperty("itemloc")]
-        public string itemloc { get; set; }
+        public string Itemloc { get; set; }
+
+        /// <summary>
+        /// need_prepay	string	否	是否加入消费者保障，值为空：全部商品，1：加入消费者保障
+        /// </summary>
+        [JsonProperty("need_prepay")]
+        public string NeedPrepay { get; set; }
 
         ///<summary> 
         ///cat string	否	商品筛选-后台类目ID(category_id)。用,分割，最大10个，该ID可以加入折淘客开放平台API群来获取。cat=50014023，表示：垂钓装备。cat=29，表示：宠物/宠物食品及用品。cat=50050359，表示：水产肉类/新鲜蔬果/熟食。cat=50454031，表示：景点门票/演艺演出/周边游。cat=122650005，表示：童鞋/婴儿鞋/亲子鞋。cat=122950001，表示：节庆用品/礼品。大家可以利用此参数，制作更多的特色专题栏目，如需要更多分类，请联系折淘客QQ群技术。
